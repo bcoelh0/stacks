713f1980db589c5fbcc79ff7b83e3fbdf8144cd6
bcoelh0 <brunocsncoelho@gmail.com>
2022-11-22 10:50:55 +0000

    Updates contract and adds tests

--- contracts/WhitelistStacks.sol
+++ contracts/WhitelistStacks.sol
@@ -23,6 +23,7 @@ contract WhitelistStacks is Ownable {
   mapping(address => bool) public registered;
   mapping(address => uint) public referralsLength;
   uint public whitelistLength;
+  uint public maxListLength = 1000;
   Account [] public whitelist;
 
   event WhitelistAdded(address indexed account);
@@ -34,21 +35,12 @@ contract WhitelistStacks is Ownable {
 
   // make payment and add sender address to whitelist with according plan
   function addToWhitelist(address _referredBy) public {
-    require(whitelistLength < 1000, "Sorry, whitelist is full");
+    require(whitelistLength < maxListLength, "Sorry, whitelist is full");
     require(!registered[msg.sender], "Address already registered for whitelist");
     require(_referredBy != msg.sender, "You cannot refer yourself");
 
-    // select correct plan
-    uint plan;
-    if(whitelistLength < 100) {
-      plan = 0;
-    } else if(whitelistLength < 250) {
-      plan = 1;
-    } else if(whitelistLength < 500) {
-      plan = 2;
-    } else {
-      plan = 3;
-    }
+    // select currect plan
+    uint plan = currentPlan();
 
     // filter out referral if not registered
     address referredBy;
@@ -77,6 +69,28 @@ contract WhitelistStacks is Ownable {
     emit ReferralFeesPaid(msg.sender);
   }
 
+  function currentPlan() public view returns(uint) {
+    if(whitelistLength < 100) {
+      return 0;
+    } else if(whitelistLength < 250) {
+      return 1;
+    } else if(whitelistLength < 500) {
+      return 2;
+    } else {
+      return 3;
+    }
+  }
+
+  function withdrawlsLeft() public view returns(uint) {
+    uint totalLeft = 0;
+    for(uint i=0; i < whitelist.length; i++){
+      if(referralsLength[whitelist[i].referredBy] > 0){
+        totalLeft++;
+      }
+    }
+    return totalLeft;
+  }
+
   function amountToPayToReferrer(address _user) public view returns(uint) {
     return referralsLength[_user] * 50 * 10 ** 18;
   }
@@ -86,8 +100,12 @@ contract WhitelistStacks is Ownable {
     openWithdrawals = _openWithdrawals;
   }
 
-  function withdrawFunds() public onlyOwner {
-    usdcAddress.transfer(msg.sender, usdcAddress.balanceOf(address(this)));
+  function withdrawFunds(address to, uint amount) public onlyOwner {
+    usdcAddress.transfer(to, amount);
+  }
+
+  function setMaxListLength(uint _maxListLength) public onlyOwner {
+    maxListLength = _maxListLength;
   }
 }
 
--- test/whitelist_stacks.js
+++ test/whitelist_stacks.js
@@ -5,28 +5,87 @@ let toWei = (n) => {
   return web3.utils.toWei(n, 'ether');
 }
 
-contract("WhitelistStacks", function (accounts) {
-  before(async function () {
+contract("WhitelistStacks", async (accounts) => {
+  let whitelistStacks, fusdc;
 
-    // c
+  before(async () => {
+    fusdc = await FUSDC.new();
+    whitelistStacks = await WhitelistStacks.new(fusdc.address);
+    await fusdc.transfer(accounts[1], toWei('1000'), { from: accounts[0] })
+  })
+
+  describe('#addToWhitelist', async () => {
+    it("use addToWhitelist to register as the first whitelist user", async () => {
+      let whitelistLength = await whitelistStacks.whitelistLength();
+      assert.equal(whitelistLength.toString(), '0', "Whitelist is 0");
+
+      await fusdc.approve(whitelistStacks.address, toWei('10000'), { from: accounts[0] });
+      await whitelistStacks.addToWhitelist('0x0000000000000000000000000000000000000000', { from: accounts[0] });
+      whitelistLength = await whitelistStacks.whitelistLength();
+      assert.equal(whitelistLength.toString(), '1', "Whitelist has been updated");
+    });
+
+    it("use addToWhitelist to register another user with a referral", async () => {
+      let whitelistLength = await whitelistStacks.whitelistLength();
+      assert.equal(whitelistLength.toString(), '1', "Whitelist count is 1");
+
+      await fusdc.approve(whitelistStacks.address, toWei('10000'), { from: accounts[1] });
+      // add acc1, referred by acc0
+      await whitelistStacks.addToWhitelist(accounts[0], { from: accounts[1] });
+      whitelistLength = await whitelistStacks.whitelistLength();
+      assert.equal(whitelistLength.toString(), '2', "Whitelist has been updated");
+
+      let referred = await whitelistStacks.referralsLength(accounts[0], { from: accounts[0] });
+      assert.equal(referred.toString(), '1', "User has referred 1 other user");
+
+      referred = await whitelistStacks.referralsLength(accounts[1], { from: accounts[0] });
+      assert.equal(referred.toString(), '0', "Acc1 has not referred anyone");
+    });
+
+    it("account tries to register with itself as a referral", async () => {
+      // shouldn't be able to
+    });
+
+    it("account tries to referral without being registered", async () => {
+      // shouldn't be able to
+    });
+
+    it("account tries to register without enough money", async () => {
+      // shouldn't be able to
+    });
+
+    it("100+ users register and this changes the price on the WL", async () => {
+    });
   });
 
-  it("should assert true", async function () {
-    await WhitelistStacks.deployed();
-    return assert.isTrue(true);
+  describe('#receiveReferral', async () => {
+    it("user cannot withdraw if openWithdrawals is false", async () => {
+    });
+
+    it("user cannot receive money if they didn't refer anyone", async () => {
+    });
+
+    it("user refers 2 users and can receive 100 USDC", async () => {
+    });
+
+    it("user refers 2 users but cant claim it twice", async () => {
+    });
   });
 
-  // use addToWhitelist to register as the first whitelist user
-  it("use addToWhitelist to register as the first whitelist user", async function () {
-    const whitelistStacks = await WhitelistStacks.deployed();
-    const fusdc = await FUSDC.deployed();
+  describe('#amountToPayToReferrer', async () => {
+    it("user has 10 referrals", async () => {
+    });
 
-    let whitelistLength = await whitelistStacks.whitelistLength();
-    assert.equal(whitelistLength.toString(), '0', "Whitelist is 0");
+    it("user has 5 referrals", async () => {
+    });
 
-    await fusdc.approve(whitelistStacks.address, toWei('10000'), { from: accounts[0] });
-    await whitelistStacks.addToWhitelist('0x0000000000000000000000000000000000000000', { from: accounts[0] });
-    whitelistLength = await whitelistStacks.whitelistLength();
-    assert.equal(whitelistLength.toString(), '1', "Whitelist has been updated");
+    it("user has 3 referrals", async () => {
+    });
+
+    it("user has 1 referral", async () => {
+    });
+
+    it("user has 0 referrals", async () => {
+    });
   });
 });
