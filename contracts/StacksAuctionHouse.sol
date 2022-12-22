5f9b2af699a62e04414280449bf3649b36069f08
bcoelh0 <brunocsncoelho@gmail.com>
2022-12-22 09:02:01 +0000

    Improves on Stacks NFT contract

---
+++ contracts/StacksAuctionHouse.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StacksAuctionHouse is Ownable {
+
+
+
+  constructor() {}
+
+
+  // addAvailableForMint
+  // prepTokenMetadata
+}
--- contracts/stacks.sol
+++ contracts/stacks.sol
@@ -5,10 +5,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract Stacks is ERC721, Ownable {
+  // Todo:
+  // - have a 33.3% tax if sold below X value for the treasury
+  // - have a 10% tax for the treasury
+  // add metadata
+
   mapping(uint => address) public tokenOwnershipRecord;
   mapping(uint => tokenMetaData) public tokenMetaDataRecord;
 
   address public auctionHouse;
+  bool public mintOpen;
+  uint public maxSupply = 5000; // Immutable
+  uint public softCap = 4009;
+  uint public teamMinted = 0;
 
   struct tokenMetaData {
     bool minted;
@@ -20,6 +29,8 @@ contract Stacks is ERC721, Ownable {
   constructor() ERC721("Stacks", "STACKS") {}
 
   function mint(uint tokenId) public {
+    require(mintOpen, "Minting is not open");
+    require(tokenId < maxSupply, "Token ID is too high");
     require(tokenOwnershipRecord[tokenId] == msg.sender, "You are not allowed to mint this token");
     require(!tokenMetaDataRecord[tokenId].minted, "Token has already been minted");
 
@@ -47,5 +58,28 @@ contract Stacks is ERC721, Ownable {
   // Admin functions
   function setAuctionHouse(address _auctionHouse) public onlyOwner {
     auctionHouse = _auctionHouse;
+  }
+
+  function toggleMint(bool open) public onlyOwner {
+    mintOpen = open;
+  }
+
+  // Mint token to offer new team members
+  function teamMint(uint tokenId, string memory metadataURI, address teamMember) public onlyOwner {
+    // tokenId for team must be between 4009 and 5000
+    require(tokenId > softCap, "Token ID too low");
+    require(tokenId < maxSupply, "Token ID is too high");
+    // addAvailableForMint
+    tokenOwnershipRecord[tokenId] = teamMember;
+    // prepTokenMetadata
+    tokenMetaDataRecord[tokenId] = tokenMetaData({
+      minted: true,
+      tokenId: tokenId,
+      timestamp: block.timestamp,
+      metadataURI: metadataURI
+    });
+    // mint
+    _safeMint(msg.sender, tokenId);
+    teamMinted++;
   }
 }
