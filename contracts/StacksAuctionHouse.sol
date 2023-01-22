6421f36dbc68543e11716d033e6702996166c940
bcoelh0 <brunocsncoelho@gmail.com>
2023-01-22 19:06:21 +0000

    Builds most of AuctionHouse contract

--- contracts/StacksAuctionHouse.sol
+++ contracts/StacksAuctionHouse.sol
@@ -2,14 +2,96 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./Stacks.sol";
 
 contract StacksAuctionHouse is Ownable {
+  Stacks public stacks;
+  mapping(uint => Auction) public auctions;
+  mapping(uint => address payable) public auctionWinner;
+  uint public totalAuctions = 0;
+  address payable public treasuryAddress;
 
+  struct Auction {
+    uint endPrice;
+    uint startTime;
+    uint endTime;
+    bool active;
+  }
 
+  constructor(address _stacksAddress, address payable _treasuryAddress) {
+    stacks = Stacks(_stacksAddress);
+    treasuryAddress = _treasuryAddress;
 
-  constructor() {}
+    uint startTime = 1677628800; // Wed Mar 01 2023 00:00:00 GMT+0000
+    // Create the first auction
+    auctions[0] = Auction({
+      endPrice: 0,
+      startTime: startTime,
+      endTime: startTime + 1 days,
+      active: true
+    });
+    // Create the second auction
+    startAuction(1);
+  }
 
+  function getCurrentAuction() public view returns (Auction memory) {
+    return auctions[totalAuctions];
+  }
 
-  // addAvailableForMint
-  // prepTokenMetadata
+  function getAuction(uint _tokenId) public view returns (Auction memory) {
+    return auctions[_tokenId];
+  }
+
+  function bid() public payable {
+    Auction memory auction = getCurrentAuction();
+    require(msg.value > auction.endPrice, "Bid is too low");
+    uint lastBid = auction.endPrice;
+    address payable lastBidder = auctionWinner[totalAuctions];
+    // update the auction
+    auctionWinner[totalAuctions] = payable(msg.sender);
+    auctions[totalAuctions].endPrice = msg.value;
+    // refund last bidder
+    lastBidder.transfer(lastBid);
+    // after-bid callback
+    if (auction.endTime < block.timestamp && auction.active) {
+      closeAuction(totalAuctions);
+    }
+  }
+
+  function closeAuction(uint _tokenId) private {
+    Auction memory auction = auctions[_tokenId];
+    // close the auction and open the next one
+    auction.active = false;
+    auctions[_tokenId + 1].active = true;
+    // create new auction ahead of the current one
+    startAuction(_tokenId + 2);
+    // total auctions gets incremented when the auction is finished
+    totalAuctions++;
+    // then winner can mint
+    allowMint(_tokenId);
+    // move funds to the treasury
+    treasuryAddress.transfer(auction.endPrice);
+  }
+
+  function startAuction(uint _tokenId) private {
+    // this must run 1 in advance of the auction.
+    // [0, 1, 2 (current), 3] --> when current moves to 3, then the 4th is created
+    Auction memory lastAuction = auctions[_tokenId - 1];
+    auctions[_tokenId] = Auction({
+      endPrice: 0,
+      startTime: lastAuction.endTime + 1, // 1 second after the last auction ends
+      endTime: lastAuction.endTime + 1 days, // 1 day after the last auction ends
+      active: false
+    });
+  }
+
+  function allowMint(uint _tokenId) private {
+    // Allow the winner to mint the token on the Stacks contract
+    stacks.addAvailableForMint(_tokenId, auctionWinner[_tokenId]);
+  }
+
+  // Admin functions
+  function setTreasuryAddress(address payable _treasuryAddress) public onlyOwner {
+    treasuryAddress = _treasuryAddress;
+  }
 }
--- contracts/stacks.sol
+++ contracts/stacks.sol
@@ -12,7 +12,7 @@ contract Stacks is ERC721, ERC2981, Ownable {
   uint public SALE_FEE = 1000;
 
   mapping(uint => address) public tokenMinter;
-  mapping(uint => tokenMetaData) public tokenMetaDataRecord;
+  mapping(uint => TokenMetaData) public tokenMetaDataRecord;
 
   address public auctionHouse;
   bool public mintOpen;
@@ -20,64 +20,56 @@ contract Stacks is ERC721, ERC2981, Ownable {
   uint public softCap = 4009;
   uint public teamMinted = 0;
 
-  struct tokenMetaData {
+  struct TokenMetaData {
     bool minted;
-    uint tokenId;
     uint timestamp;
   }
 
   constructor() ERC721("Stacks", "STACKS") {}
 
-  function mint(uint tokenId) public {
+  function mint(uint _tokenId) public {
     require(mintOpen, "Minting is not open");
-    require(tokenId < maxSupply, "Token ID is too high");
-    require(tokenMinter[tokenId] == msg.sender, "You are not allowed to mint this token");
-    require(!tokenMetaDataRecord[tokenId].minted, "Token has already been minted");
+    require(_tokenId <= maxSupply, "Token ID is too high");
+    require(tokenMinter[_tokenId] == msg.sender, "You are not allowed to mint this token");
+    require(!tokenMetaDataRecord[_tokenId].minted, "Token has already been minted");
 
-    _safeMint(msg.sender, tokenId);
-    tokenMetaDataRecord[tokenId].minted = true;
+    _safeMint(msg.sender, _tokenId);
+    // tokenMetaDataRecord[_tokenId].minted = true;
+    tokenMetaDataRecord[_tokenId] = TokenMetaData(true, block.timestamp);
   }
 
   // Set who can mint the token with tokenId
-  function addAvailableForMint(uint tokenId, address auctionWinner) public {
+  function addAvailableForMint(uint _tokenId, address _auctionWinner) public {
     require(msg.sender == auctionHouse, "You are not the auction house");
-    tokenMinter[tokenId] = auctionWinner;
+    tokenMinter[_tokenId] = _auctionWinner;
   }
 
   // Runs before the auction so the auction house can set the metadata
-  function prepTokenMetadata(uint tokenId) public {
-    require(msg.sender == auctionHouse, "You are not the auction house");
-    tokenMetaDataRecord[tokenId] = tokenMetaData({
-      minted: false,
-      tokenId: tokenId,
-      timestamp: block.timestamp
-    });
-  }
+  // function prepTokenMetadata(uint tokenId) public {
+  //   require(msg.sender == auctionHouse, "You are not the auction house");
+  //   tokenMetaDataRecord[tokenId] = TokenMetaData(false, block.timestamp);
+  // }
 
   // Admin functions
   function setAuctionHouse(address _auctionHouse) public onlyOwner {
     auctionHouse = _auctionHouse;
   }
 
-  function toggleMint(bool open) public onlyOwner {
-    mintOpen = open;
+  function toggleMint(bool _open) public onlyOwner {
+    mintOpen = _open;
   }
 
   // Mint token to offer new team members
-  function teamMint(uint tokenId, address teamMember) public onlyOwner {
-    // tokenId for team must be between 4009 and 5000
-    require(tokenId > softCap, "Token ID too low");
-    require(tokenId < maxSupply, "Token ID is too high");
+  function teamMint(uint _tokenId, address _teamMember) public onlyOwner {
+    // _tokenId for team must be between 4010 and 5000
+    require(_tokenId > softCap, "Token ID too low");
+    require(_tokenId <= maxSupply, "Token ID is too high");
     // addAvailableForMint
-    tokenMinter[tokenId] = teamMember;
+    tokenMinter[_tokenId] = _teamMember;
     // prepTokenMetadata
-    tokenMetaDataRecord[tokenId] = tokenMetaData({
-      minted: true,
-      tokenId: tokenId,
-      timestamp: block.timestamp
-    });
+    tokenMetaDataRecord[_tokenId] = TokenMetaData(true, block.timestamp);
     // mint
-    _safeMint(msg.sender, tokenId);
+    _safeMint(msg.sender, _tokenId);
     teamMinted++;
   }
 
@@ -86,18 +78,23 @@ contract Stacks is ERC721, ERC2981, Ownable {
   }
 
   function supportsInterface(bytes4 _interfaceId) public view virtual override(ERC721, ERC2981) returns (bool) {
-    if (_interfaceId == _INTERFACE_ID_ERC2981) {
-      return true;
-    }
+    if (_interfaceId == _INTERFACE_ID_ERC2981) { return true; }
     return super.supportsInterface(_interfaceId);
   }
 
-  // Overwrite BaseURI
-  function _baseURI() override view internal returns (string memory) {
-    return "ipfs://stacks...";
+  function royaltyInfo(uint256, uint256 _salePrice) override view public returns (address receiver, uint256 royaltyAmount){
+    return (owner(), _salePrice * SALE_FEE / PERCENT_DIVIDER);
   }
 
-  function royaltyInfo(uint256 _tokenId, uint256 _salePrice) override view public returns (address receiver, uint256 royaltyAmount){
-    return (owner(), _salePrice * SALE_FEE / PERCENT_DIVIDER);
+  // Overwrite tokenURI
+  // ignore tokenId as all the tokens have the same image
+  function tokenURI(uint256 _tokenId) public view virtual override returns (string memory) {
+    _requireMinted(_tokenId);
+    return string(abi.encodePacked(_baseURI()));
+  }
+
+  // Overwrite _baseURI
+  function _baseURI() override pure internal returns (string memory) {
+    return "ipfs://QmY2aPCZQNvsZUQvUS2g6cCSmdLXuhrHnRUEcoLfwy9HW4";
   }
 }
